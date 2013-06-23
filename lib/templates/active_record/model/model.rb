9ffb14e71dd0e9ced4c98078c2432916ebd483da
lee <leebrooks0@gmail.com>
2013-06-23 19:34:01 +0200

    Configured PaperTrail

--- lib/templates/active_record/model/model.rb
+++ lib/templates/active_record/model/model.rb
@@ -3,6 +3,7 @@ Lee custom model
 class <%= class_name %> < <%= parent_class_name.classify %>
   acts_as_paranoid
   strip_attributes
+  has_paper_trail
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
 <% end -%>
